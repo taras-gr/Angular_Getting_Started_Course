74f40d70776c7a8f38977edd0607d9645e8665d1
taras-gr <tar.grytsenko@gmail.com>
2024-01-23 10:58:53 +0200

    Module 5: 	Templates, Interpolation, and Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from "@angular/core";
+import { ProductListComponent } from "./products/product-list.component";
 
 @Component({
   selector: 'pm-root',
   standalone: true,
+  imports: [ProductListComponent],
   template: `
   <div><h1>{{pageTitile}}</h1>
-    <div>My First Component</div>
+    <pm-products></pm-products>
   </div>
   `
 })
---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'pm-products',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-list.component.html'
+})
+export class ProductListComponent {
+  pageTitle: string = 'Product List';
+  products: any[] = [
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2021",
+      "description": "15 gallon capacity description",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "assets/images/garden_cart.png"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-1123",
+      "releaseDate": "May 18, 2022",
+      "description": "Curved steel tool",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "assets/images/hammer.png"
+    }
+  ];
+}
