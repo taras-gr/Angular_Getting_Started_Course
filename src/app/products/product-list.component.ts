5ffc9eae4db2cd9428ba4b5e6e9e2dc3bdaeb30a
taras-gr <tar.grytsenko@gmail.com>
2024-01-25 19:24:47 +0200

    Module 8: Building Nested Components

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -65,4 +65,8 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
     this.listFilter = 'cart';
   }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
 }
