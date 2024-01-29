fc4def8df73d0e01242932f98f5c18b9f16c2216
taras-gr <tar.grytsenko@gmail.com>
2024-01-29 19:01:55 +0200

    Module 9: Services and Dependency Injection

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from "@angular/forms";
 import { IProduct } from "./product";
 import { ConvertToSpacesPipe } from "../shared/convert-to-spaces.pipe";
 import { StarComponent } from "../shared/star.component";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
@@ -29,28 +30,9 @@ export class ProductListComponent implements OnInit {
   }
 
   filteredProducts: IProduct[] = [];
-  products: IProduct[] = [
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity description",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/garden_cart.png"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-1123",
-      "releaseDate": "May 18, 2022",
-      "description": "Curved steel tool",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    }
-  ];
+  products: IProduct[] = [];
+
+  constructor(private productService: ProductService) {}
 
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
@@ -63,7 +45,8 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.listFilter = 'cart';
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 
   onRatingClicked(message: string): void {
---
+++ src/app/products/product.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { IProduct } from "./product";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  getProducts(): IProduct[] {
+    return [
+      {
+        "productId": 2,
+        "productName": "Garden Cart",
+        "productCode": "GDN-0023",
+        "releaseDate": "March 18, 2021",
+        "description": "15 gallon capacity description",
+        "price": 32.99,
+        "starRating": 4.2,
+        "imageUrl": "assets/images/garden_cart.png"
+      },
+      {
+        "productId": 5,
+        "productName": "Hammer",
+        "productCode": "TBX-1123",
+        "releaseDate": "May 18, 2022",
+        "description": "Curved steel tool",
+        "price": 8.9,
+        "starRating": 4.8,
+        "imageUrl": "assets/images/hammer.png"
+      }
+    ];
+  }
+}
